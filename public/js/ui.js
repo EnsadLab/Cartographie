2cc4c4a1bd76493e6b3fc654f9f769fde7fbb945
AlexDCA348 <alexandre.dechosal@free.fr>
2019-04-26 10:13:40 +0200

    1st ui

--- public/js/ui.js
+++ public/js/ui.js
@@ -4,3 +4,11 @@
 d3.select("#testbutton1").on("click", applyTest1 )
 d3.select("#testbutton2").on("click", applyTest2 )
 d3.select("#testbutton3").on("click", applyTest3 )
+
+
+// Search input
+var options = {
+    valueNames: [ 'revue-title' ]
+};
+
+var hackerList = new List('menu', options);
